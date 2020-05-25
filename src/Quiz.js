e3d27cbedfa63abf72d62f2bf8dda153c3dce79f
Christoph Uhland <42832096+cuhland@users.noreply.github.com>
2020-05-25 13:47:41 +0200

    tada

--- src/Quiz.js
+++ src/Quiz.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 let quizData = require('./quiz_data.json')
 
-class Quiz extends React.Component {
+class Quiz extends Component {
 
     constructor(props) {
         super(props);
@@ -14,7 +14,11 @@ class Quiz extends React.Component {
 
     render(){
         return(
-            <div>Quiz</div>
+            <div>
+                <div className="QuizQuestion">
+                    {quizData.quiz_questions[0].instruction_text}
+                </div>
+            </div>
         )
     }
 }
