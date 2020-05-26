ee0f52adeaafba4bb7d292eaa3d43113e899bbd8
Christoph Uhland <42832096+cuhland@users.noreply.github.com>
2020-05-26 10:34:18 +0200

    Update for Testcase

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,7 +12,10 @@ class Quiz extends Component {
     }
   }
   render() {
-    const isQuizEnd = true
+    const isQuizEnd =
+this.state.quiz_position - 1 === quizData.quiz_questions.length;
+
+
 
     return (
       <div>
